04c6fa09c2895119447cd90d9fc5aeb39803e88d
ngocdaothanh <ngocdaothanh@daa11dfc-8091-11dd-8d1c-71c7e1663bfb>
2008-10-16 03:34:34 +0000

    Add .mqo loader

--- trunk/config.rb
+++ trunk/config.rb
@@ -2,16 +2,18 @@ CONFIG = {
   :window_width  => 640,
   :window_height => 480,
 
+  :model => 'scene',
+
   :cameras => [
     {
-      :position     => Vector[0, 5, -5],
+      :position     => Vector[0, 2, -5],
       :focal_vector => Vector[1, -0.5, 1],
       :width        => 0.64,
       :height       => 0.48,
       :visualizer   => AnimationVisualizer
     },
     {
-      :position     => Vector[-0.1, 5, -2],
+      :position     => Vector[-0.1, 2, -2],
       :focal_vector => Vector[-0.5, -0.5, -0.5],
       :width        => 0.64,
       :height       => 0.48,
