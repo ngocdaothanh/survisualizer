a36e681c4b4e2b1663cd298487e0e8fca58d161f
ngocdaothanh <ngocdaothanh@daa11dfc-8091-11dd-8d1c-71c7e1663bfb>
2009-01-20 13:59:22 +0000

    Add iPhone client

---
+++ trunk/client/iphone/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  Survisualizer
+//
+//  Created by Ngoc Dao on 1/20/09.
+//  Copyright VTM 2009. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+int main(int argc, char *argv[]) {
+    
+    NSAutoreleasePool * pool = [[NSAutoreleasePool alloc] init];
+    int retVal = UIApplicationMain(argc, argv, nil, nil);
+    [pool release];
+    return retVal;
+}
