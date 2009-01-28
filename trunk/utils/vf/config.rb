a6225cb9db3d47aee9a195e22d3eaf862ad14f16
ngocdaothanh <ngocdaothanh@daa11dfc-8091-11dd-8d1c-71c7e1663bfb>
2009-01-28 14:18:05 +0000

    Add "raw" utility

--- trunk/utils/vf/config.rb
+++ trunk/utils/vf/config.rb
@@ -7,27 +7,27 @@ CONFIG = {
   :segments_per_edge => 20,
   :cameras => [
     # L building, towards the gym
-    {
-      :position => [1, 5 - ORIGIN_HEIGHT, 58],
-      :focus    => [0.1, -0.4, -0.5],
-      :width    => 0.64,
-      :height   => 0.48
-    },
+    # {
+    #       :position => [1, 5 - ORIGIN_HEIGHT, 58.2],
+    #       :focus    => [0.1, -0.4, -0.5],
+    #       :width    => 0.64,
+    #       :height   => 0.48
+    #     },
 
     # L building, towards the parking
     {
-      :position => [-10, 5 - ORIGIN_HEIGHT, 76.5],
-      :focus    => [-0.2, -0.4, 0.5],
+      :position => [-10, 5 - ORIGIN_HEIGHT, 76.2],
+      :focus    => [-0.15, -0.3, 0.4],
       :width    => 0.64,
       :height   => 0.48
     },
 
     # D building
-    {
-      :position => [22, 5 - ORIGIN_HEIGHT, 14],
-      :focus    => [-0.5, -0.4, 0],
-      :width    => 0.64,
-      :height   => 0.48
-    }
+    # {
+    #       :position => [22.6, 5 - ORIGIN_HEIGHT, 14],
+    #       :focus    => [-0.5, -0.4, 0],
+    #       :width    => 0.64,
+    #       :height   => 0.48
+    #     }
   ]
 }
